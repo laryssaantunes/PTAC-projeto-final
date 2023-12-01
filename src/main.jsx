194dcea66392d7a8a7d48cea49e70051c987d59e
laryssaantunes <larissa.antunes.icloud@gmail.com>
2023-12-01 13:52:58 -0400

    commit tentei colocar a minha navibar mas mudei de ideia

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from './Home/Home';
+import Cadastrar from './Cadastrar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
