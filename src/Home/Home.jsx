a2e6772a6016ed782bf9b897976d4e3347281fbc
laryssaantunes <larissa.antunes.icloud@gmail.com>
2023-12-06 09:13:05 -0400

    commit com a aludas da minha amiguinha acrescentei a parte do home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 import Header from "./Compoments/Header"
-import "./Home.css";
+import Card from "./Componentes/Card";
 
 export default function Home() {
+  const listinhamusic = JSON.parse(localStorage.getItem ("Lista")) || []
 
+  return(
+    <div> 
+      <Header/>
+      <Card listinhamusic ={listinhamusic}/>
+    </div>
+  )
   
   );
 }
